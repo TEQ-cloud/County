8a7ad3a31e13ab755d9ec372f49f5fd2cef6dd51
Quinten de Haard <quintenmjfl@proton.me>
2026-03-19 15:51:58 +0100

    Initial Commit

---
+++ CountyTests/CountyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountyTests.swift
+//  CountyTests
+//
+//  Created by Quinten de Haard on 19/03/2026.
+//
+
+import Testing
+@testable import County
+
+struct CountyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
